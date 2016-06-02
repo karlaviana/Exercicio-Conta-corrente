c2f6718fcd8a891ad3e06f13ff7742e4d2a4ec52
karlaviana <karlavianacosta@gmail.com>
2016-06-02 11:15:55 -0300

    Atualizaçao da classe banco
    atualização da classe banco

--- src/br/contacorrente/Banco.java
+++ src/br/contacorrente/Banco.java
@@ -1,39 +1,31 @@
 package br.com.movimentacao;
 
-public class Movimentacao {
-	private String descricao;
-	private double valor;
-	private String informação;
-	public String getDescricao() {
-		return descricao;
-	}
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-	public double getValor() {
-		return valor;
-	}
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
-	public String getInformação() {
-		return informação;
-	}
-	public void setInformação(String informação) {
-		this.informação = informação;
-	}
-	
-	//Metodos da classe movimentacao
-	
-	public void obterCredito() {
-		 double credito = 0;
-	      credito= +valor;
-	}
-	public void obterDebito(){
-		double debito = 0;
-		debito =+valor;
-	}
-	
-	
+import java.util.ArrayList;
 
+public class Banco {
+	private ArrayList conta;
+
+	public Banco() {
+		this.conta =conta;
+	}
+
+	public ArrayList getContas() {
+		return conta;
+	}
+
+	public void setContas(ArrayList conta) {
+		this.conta = conta;
+	}
+	
+	// metodo da classe Banco
+	
+	public void criarconta(){
+		Conta conta = new Conta();
+		conta.setNumero(12345);
+		conta.setSaldo(12.00);
+		conta.setStatus('S');
+		
+	}
+	
+	
 }
