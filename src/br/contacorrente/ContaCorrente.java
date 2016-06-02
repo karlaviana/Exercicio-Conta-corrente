a2caddeb0b5ea23dfe58fb9d338987a355424cef
karlaviana <karlavianacosta@gmail.com>
2016-06-02 11:11:14 -0300

    Atualizaçao da classe conta corrente
    Atualizada a classe conta corrente

--- src/br/contacorrente/ContaCorrente.java
+++ src/br/contacorrente/ContaCorrente.java
@@ -1,68 +1,44 @@
-package br.contacorrente;
+package br.com.movimentacao;
 
-import java.util.ArrayList;
-
-public class ContaCorrente {
+public class Conta {
+	private  int numero;
 	private double saldo;
-	private int numero;
-	private double limite;
 	private char status;
-	private ArrayList<ContaCorrente> conta;
+	private double limite;
+	private int conta ;
 	
+	public Conta(){
 	
-	public ContaCorrente(double saldo,int numero,char status, double limite, ArrayList<ContaCorrente>conta) {
-		this.saldo= saldo;
-		this.limite= limite;
-		this.status= status;
-		this.numero=numero;
-		this.conta=conta;
-		
 	}
-
-	public ContaCorrente(double saldo2, int numero2, double limite2, char status2, ArrayList<ContaCorrente> conta2) {
-		
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-
 	public int getNumero() {
 		return numero;
 	}
-
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
-
-	public double getLimite() {
-		return limite;
+	public double getSaldo() {
+		return saldo;
 	}
-
-	public void setLimite(double limite) {
-		this.limite = limite;
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
 	}
-
 	public char getStatus() {
 		return status;
 	}
-
 	public void setStatus(char status) {
 		this.status = status;
 	}
-
-	public ArrayList<ContaCorrente> getConta() {
+	public double getLimite() {
+		return limite;
+	}
+	public void setLimite(double limite) {
+		this.limite = limite;
+	}
+	public int getConta() {
 		return conta;
 	}
-
-	public void setConta(ArrayList<ContaCorrente> conta) {
-		this.conta = conta;
+	public void setConta(int i) {
+		this.conta = i;
 	}
-
-
 
 }
